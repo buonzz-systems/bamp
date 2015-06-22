584b163c45fe4ea8152008fe46bf30cec1a2a2c8
buonzz <buonzz@gmail.com>
2015-06-22 17:36:41 +0800

    fixed memcache loading

--- provision.sh
+++ provision.sh
@@ -1,3 +1,7 @@
 
+# fix PHP Warning:  Module 'memcache' already loaded in Unknown on line 0
+sudo echo ";extension=memcache.so" > /etc/php5/cli/conf.d/memcache.ini
+
+# update composer to latest version
 echo "Updating Composer to latest version"
 sudo composer self-update
