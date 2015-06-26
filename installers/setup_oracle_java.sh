fae75e5e57ffc06c4b112b021e3e3dd98699266c
buonzz <buonzz@gmail.com>
2015-06-26 20:04:52 +0800

    make vm restart after setting java_home

--- installers/setup_oracle_java.sh
+++ installers/setup_oracle_java.sh
@@ -9,8 +9,10 @@ sudo debconf-set-selections
 sudo apt-get install oracle-java7-installer  -y
 
 #setup java_home environment var and save it in bash
+JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:bin/javac::")
 
-export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:bin/javac::")
+# append it in the global environment so it will be set even after restarts
+echo 'JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:bin/javac::")' | sudo tee  /etc/profile.d/bamp1.sh
 
-# append it in the global environment 
-echo 'export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:bin/javac::")' | sudo tee  /etc/profile.d/bamp1.sh 
+# restart vm for environment vars to take effect
+sudo shutdown -r now
